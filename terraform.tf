1429bb59e11b558d630dea3010fa91d7a099e5b2
vikulrepo <111678435+vikulrepo@users.noreply.github.com>
2023-04-25 21:04:00 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" // set the AMI ID of the image you want to use
+  ami           = "ami-02eb7a4783e7e9317" // set the AMI ID of the image you want to use
   instance_type = "t2.micro" // set the instance type you want to use
   key_name      = "AssignmentKey" // set the name of the key pair you want to use to SSH into the instance
   tags = {
