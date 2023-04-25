5dc2592f9ed164b67af987ae12182a7cc9fb9961
vikulrepo <111678435+vikulrepo@users.noreply.github.com>
2023-04-25 20:56:08 +0530

    new

---
+++ terraform.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1" // set the AWS region you want to use
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" // set the AMI ID of the image you want to use
+  instance_type = "t2.micro" // set the instance type you want to use
+  key_name      = "AssignmentKey" // set the name of the key pair you want to use to SSH into the instance
+  tags = {
+    Name = "vikul-instance" // set a name for the instance
+  }
+}
